4a6efcde5ba8dc19dd23ebf90a87cd6f318c31e1
dashattit <dashyla.2021@mail.ru>
2025-03-10 18:27:55 +0700

    classes and styles

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ let app = new Vue({
         product: "Socks",
         image: "./assets/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
-        inStock: true,
+        inStock: false,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
@@ -27,9 +27,9 @@ let app = new Vue({
         updateProduct(variantImage) {
             this.image = variantImage
         }
-
     }
-
 })
+
+
 
 
